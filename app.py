2e1225ebba08906c7ec6cb336dbc5023ea46193d
Naphong Chadha <harrajc@gmail.com>
2022-12-10 01:49:22 +0700

    Linked to database

--- app.py
+++ app.py
@@ -1,10 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "Never push this line to github public repo"
+
+cred = yaml.load(open('cred.yaml'), Loader=yaml.Loader)
+app.config['MYSQL_HOST'] = cred['mysql_host']
+app.config['MYSQL_USER'] = cred['mysql_user']
+app.config['MYSQL_PASSWORD'] = cred['mysql_password']
+app.config['MYSQL_DB'] = cred['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        userDetails = request.form
+
+        # Check the password and confirm password
+        if userDetails['password'] != userDetails['confirmPassword']:
+            flash('Passwords do not match!', 'danger')
+            return render_template('register.html')
+
+		#if len(userDetails['studentID']) != 7:
+		#	flash('Student ID is not a registered studentID', 'danger')
+		#	return render_template('register.html')
+
+        p1 = userDetails['firstName']
+        p2 = userDetails['lastName']
+        p3 = userDetails['studentID']
+        p4 = userDetails['password']
+
+        hashed_pw = generate_password_hash(p4)
+        print(p1 + "," + p2 + "," + p3 + "," + p4 + "," + hashed_pw)
+
+        queryStatement = (
+            f"INSERT INTO "
+            f"users(f_name, l_name, studentID, password, admin_role) "
+            f"VALUES('{p1}', '{p2}', '{p3}', '{hashed_pw}', False)"
+        )
+        print(check_password_hash(hashed_pw, p4))
+        print(queryStatement)
+        cur = mysql.connection.cursor()
+        cur.execute(queryStatement)
+        mysql.connection.commit()
+        cur.close()
+
+        flash("Form Submitted Successfully.", "success")
+        return redirect('/')    
+    return render_template('register.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
